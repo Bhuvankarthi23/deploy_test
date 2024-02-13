58dc03b4954c59b5f1f697ae63a64664cad981f1
Bhuvankarthi <bhuvankarthi@datawrkz.com>
2024-02-13 22:32:58 +0530

    allowed hosts

--- practice_site/settings.py
+++ practice_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--2)@p@bmqff^#hz&nqvj9&b+0kc59hv^&q97&b6zbu@$3&=a&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
