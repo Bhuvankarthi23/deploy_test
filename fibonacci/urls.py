842c45131eb5315ae4d972f705db45b770896d27
Bhuvankarthi <bhuvankarthi@datawrkz.com>
2024-02-14 23:29:06 +0530

    added offset and limit to the url and number validation

--- fibonacci/urls.py
+++ fibonacci/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import Fibonacci
 
 urlpatterns = [
-    path("number/<int:fibonacci_number>/",Fibonacci.as_view({"get": 'get'}) , name="index"),
+    path("number/<str:fibonacci_number>/",Fibonacci.as_view({"get": 'get'}) , name="index"),
 ]
