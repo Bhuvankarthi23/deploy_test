ecec316a6e19ee561bff3e00d7d33ca9bce97d75
Bhuvankarthi <bhuvankarthi@datawrkz.com>
2024-02-14 15:32:04 +0530

    added static file directory path in settings

--- practice_site/settings.py
+++ practice_site/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Application definition
 
