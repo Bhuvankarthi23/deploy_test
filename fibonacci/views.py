e8d9cb0b4c790cf285d6b4ceefeb85eb3f646d68
Bhuvankarthi <bhuvankarthi@datawrkz.com>
2024-02-15 11:01:57 +0530

    removed model import

--- fibonacci/views.py
+++ fibonacci/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from .models import Fibonacci
 
 class Fibonacci(viewsets.ViewSet):
     pagination_class = LimitOffsetPagination
